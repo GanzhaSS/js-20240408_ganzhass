ffe4cf3f6f54a93ee1d1b3b8f3792d4c1ebabfd1
ganzhass <ganzhass@nornik.ru>
2024-04-22 00:06:08 +0300

    3.3 not working

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,31 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+function trimSymbols(string, size) {
+  let newStr = "";
+  countSameLetter = 0;
 
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i + 1]) {
+      countSameLetter++;
+      //   console.log(string[i]);
+      //   console.log(countSameLetter);
+      if (countSameLetter < size) {
+        newStr += string[i];
+        // console.log(newStr);
+      }
+    } else {
+      newStr += string[i];
+      countSameLetter = 1;
+    }
+  }
+  return newStr;
 }
+
+// trimSymbols("x", 3); // 'xxx' - ничего не удалили т.к разрешено 3 символа подряд
+
+console.log(trimSymbols("ффффффффоооооooooooo", 3)); // 'xx' - удалили один символ
+// trimSymbols("xxx", 1); // 'x'
+
+// trimSymbols("xxxaaaaa", 2); // 'xxaa'
+// trimSymbols("xxxaaaaab", 3); // 'xxxaaab'
